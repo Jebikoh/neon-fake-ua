aa4eabed72dd394fd3610ed2fdf17a670209345b
Jayden Edara <aedara@uw.edu>
2023-08-04 18:36:25 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='scrapy-fake-useragent',
+    name='neon-fake-ua',
 
-    version='1.4.4',
+    version='1.0.0',
 
     description='Use a random User-Agent provided by fake-useragent '
-                'for every request',
+                'for every request unless playwright is active',
     long_description=open('README.rst').read(),
 
     keywords='scrapy proxy user-agent web-scraping',
-    license='New BSD License',
+    license='MIT License',
 
     author='Alexander Afanasyev',
     author_email='me@alecxe.me',
-    maintainer='Alexander Afanasyev',
-    maintainer_email='me@alecxe.me',
+    maintainer='Jayden Edara',
+    maintainer_email='aedara@uw.edu',
 
-    url='https://github.com/alecxe/scrapy-fake-useragent',
+    url='https://github.com/alecxe/neon-fake-ua',
     python_requires=">=3.5",
 
     classifiers=[
@@ -34,7 +34,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
     ],
     packages=[
-        'scrapy_fake_useragent',
+        'neon_fake_ua',
     ],
     install_requires=[
         'fake-useragent',
